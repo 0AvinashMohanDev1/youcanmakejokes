62d56eb7541c15392601bfa4f10cc860b660af48
0AvinashMohanDev1 <avinashmohandev@gmail.com>
2023-12-03 17:44:02 +0530

    done

--- README.md
+++ README.md
@@ -1,3 +1,63 @@
 # youcanmakejokes
-node index.js searchTerm term(like friend or data or etc.)
+
+This Node.js application allows users to search for jokes based on a provided search term and tracks user responses to jokes, creating a leaderboard of the most liked jokes.
+
+## Prerequisites
+
+- Node.js installed on your machine
+
+## Getting Started
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone <repository-url>
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd <project-directory>
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+4. Run the application:
+
+   ```bash
+   node <filename.js> <operation> <search-term>
+   ```
+
+   - `<filename.js>`: The name of the JavaScript file (e.g., `index.js`).
+   - `<operation>`: Use `searchTerm` to search for jokes or `leaderBoard` to view the leaderboard.
+   - `<search-term>`: The word to search for jokes (applicable only for the `searchTerm` operation).
+
+## Operations
+
+### 1. Search for Jokes
+
+To search for jokes based on a specific term:
+
+```bash
+node index.js searchTerm <word>
+```
+
+Replace `<word>` with the term you want to search for jokes.
+
+### 2. View Leaderboard
+
+To view the leaderboard of the most liked jokes:
+
+```bash
 node index.js leaderBoard
+```
+
+## Feedback
+
+After each displayed joke, the application prompts the user to provide feedback by answering whether they liked the joke. The feedback is recorded, and the leaderboard is updated accordingly.
+
+---
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const fs=require("fs");
+const fs = require("fs");
 const fetch = require('node-fetch');
-const argv=process.argv.slice(2);
-const operation=argv[0];
-const readline=require("readline")
+const argv = process.argv.slice(2);
+const operation = argv[0];
+const readline = require("readline");
 
-const wordForJoke=argv[1];
+const wordForJoke = argv[1];
 
+// Switch statement to determine the operation based on user input
 switch (operation) {
     case 'searchTerm':
         jokeCrack(wordForJoke);
@@ -14,86 +15,106 @@ switch (operation) {
         topJoke();
         break;
     default:
-        console.log(`Please use {searchTerm} word for searching any jokes.`)
+        console.log(`Please use {searchTerm} word for searching any jokes.`);
         break;
 }
 
-async function jokeCrack(word){
-    
+// Function to fetch and display a joke based on a search term
+async function jokeCrack(word) {
     try {
-      const response=await  fetch(`https://icanhazdadjoke.com/search?term=${word}`,{
-        headers:{Accept:"application/json"}
-      })
-      if(!response.ok)return console.log(`Error in fetching data, ${response.status}`);
-      const data=await response.json();
-    //   console.log(data)
-      if(data.results.length>0){
-        const size=data.results.length;
-        const num=randomNum(0,size);
-        const joke=data.results[num].joke;
+        // Fetch jokes based on the provided search term
+        const response = await fetch(`https://icanhazdadjoke.com/search?term=${word}`, {
+            headers: { Accept: "application/json" }
+        });
 
-        fs.appendFileSync("jokes.txt",`${joke}\n`);
-        console.log(joke);
-        // Ask the user if they like the joke
-      const userResponse = await getUserResponse();
+        // Check if the response is successful
+        if (!response.ok) return console.log(`Error in fetching data, ${response.status}`);
 
-      // Process the user response and update the leaderboard
-      updateLeaderboard(joke, userResponse);
-      }else {
-                throw new Error(`No jokes found for "${word}"`);
-      }
+        // Parse the response to JSON
+        const data = await response.json();
+
+        // If jokes are found, select a random one and display it
+        if (data.results.length > 0) {
+            const size = data.results.length;
+            const num = randomNum(0, size);
+            const joke = data.results[num].joke;
+
+            // Append the joke to a file
+            fs.appendFileSync("jokes.txt", `${joke}\n`);
+
+            // Display the joke
+            console.log(joke);
+
+            // Ask the user for feedback and update the leaderboard
+            const userResponse = await getUserResponse();
+            updateLeaderboard(joke, userResponse);
+        } else {
+            throw new Error(`No jokes found for "${word}"`);
+        }
     } catch (error) {
-        console.log({error})
-      console.log("Attention all humans! The joke gods have officially declared a day off. Apparently, even deities need a break from the pun-derful workload. In the meantime, please feel free to create your own laughter; just be cautious not to strain your humor muscles. Normal joking services will resume shortly. Thank you for your understanding, and remember, laughter is the best medicine, even when the joke gods are on vacation!");
+        console.log({ error });
+        console.log("Attention all humans! The joke gods have officially declared a day off. Apparently, even deities need a break from the pun-derful workload. In the meantime, please feel free to create your own laughter; just be cautious not to strain your humor muscles. Normal joking services will resume shortly. Thank you for your understanding, and remember, laughter is the best medicine, even when the joke gods are on vacation!");
     }
 }
+
+// Function to get user response
 const getUserResponse = async () => {
     const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
+        input: process.stdin,
+        output: process.stdout
     });
-  
+
     return new Promise(resolve => {
-      rl.question('Did you like the joke? (yes/no): ', answer => {
-        rl.close();
-        resolve(answer.toLowerCase().trim());
-      });
+        rl.question('Did you like the joke? (yes/no): ', answer => {
+            rl.close();
+            resolve(answer.toLowerCase().trim());
+        });
     });
-  };
+};
 
-
-  const updateLeaderboard = (joke, userResponse) => {
+// Function to update the leaderboard with user responses to jokes
+const updateLeaderboard = (joke, userResponse) => {
     const leaderboardPath = 'leaderboard.json';
-  
+
     try {
-      const leaderboard = fs.existsSync(leaderboardPath)
-        ? JSON.parse(fs.readFileSync(leaderboardPath, 'utf8'))
-        : {};
-      leaderboard[joke] = leaderboard[joke] || { likes: 0, dislikes: 0 };
-  
-      userResponse === 'yes' ? leaderboard[joke].likes++ : leaderboard[joke].dislikes++;
-  
-      fs.writeFileSync(leaderboardPath, JSON.stringify(leaderboard, null, 2));
-  
-      console.log(`User response recorded. Thank you for your feedback!`);
+        // Read existing leaderboard data or initialize an empty object
+        const leaderboard = fs.existsSync(leaderboardPath)
+            ? JSON.parse(fs.readFileSync(leaderboardPath, 'utf8'))
+            : {};
+
+        // Ensure the joke has an entry in the leaderboard
+        leaderboard[joke] = leaderboard[joke] || { likes: 0, dislikes: 0 };
+
+        // Update the count based on user response
+        userResponse === 'yes' ? leaderboard[joke].likes++ : leaderboard[joke].dislikes++;
+
+        // Write the updated leaderboard to the file
+        fs.writeFileSync(leaderboardPath, JSON.stringify(leaderboard, null, 2));
+
+        console.log(`User response recorded. Thank you for your feedback!`);
     } catch (error) {
-      console.error('Error updating leaderboard:', error.message);
+        console.error('Error updating leaderboard:', error.message);
     }
-  };
-  
- async function topJoke(){
-    const response=fs.readFileSync('leaderboard.json','utf-8');
-    const jokes=JSON.parse(response);
-    let lead='not fund',num=0;
-    for(let joke in jokes){
-        if(jokes[joke].likes>num){
-            lead=joke;
-            num=jokes[joke].likes;
+};
+
+// Function to display the joke with the highest likes from the leaderboard
+async function topJoke() {
+    const response = fs.readFileSync('leaderboard.json', 'utf-8');
+    const jokes = JSON.parse(response);
+    let lead = 'not found', num = 0;
+
+    // Iterate through the jokes to find the one with the highest likes
+    for (let joke in jokes) {
+        if (jokes[joke].likes > num) {
+            lead = joke;
+            num = jokes[joke].likes;
         }
     }
-    console.log(lead);
- }
 
+    console.log(lead);
+}
+
+// Function to generate a random number within a specified range
 function randomNum(min, max) {
-	return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
